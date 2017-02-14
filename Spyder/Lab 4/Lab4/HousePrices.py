38111bf4cb36af4f1360b287a52f14f90805c7f2
Atreya Misra <atreyamisra@Atreyas-MacBook-Pro.local>
2017-02-14 12:53:46 -0600

    lasso and ridge finsihed and on kaggle

--- Spyder/Lab 4/Lab4/HousePrices.py
+++ Spyder/Lab 4/Lab4/HousePrices.py
@@ -17,8 +17,9 @@ from PIL import Image
 from scipy.misc import imread
 from scipy.stats import skew
 from scipy.stats.stats import pearsonr
-from sklearn.linear_model import Ridge, LassoCV
+from sklearn.linear_model import Ridge, LassoCV, Lasso
 from sklearn.model_selection import cross_val_score
+import xgboost as xgb
 
 train = pd.read_csv("train.csv")
 test = pd.read_csv("test.csv")
@@ -58,16 +59,26 @@ def ridgeRegTest(a):
     predict = np.expm1(clf.predict(X_test))
 
     results = pd.DataFrame({"Id":test.Id, "SalePrice":predict})
-    results.to_csv("results.csv", index = False)
+    results.to_csv("RidgeResults.csv", index = False)
 
     results.plot(x='Id', y='SalePrice', kind='line')
     plt.show()
-#ridgeRegTest(.1)
+#ridgeRegTest(10.10077)
+def lassoTest(a):
+    clf = Lasso(alpha=a)
+    clf.fit(X_train, y)
+    predict = np.expm1(clf.predict(X_test))
+
+    results = pd.DataFrame({"Id":test.Id, "SalePrice":predict})
+    results.to_csv("LassoResults.csv", index = False)
+
+    results.plot(x='Id', y='SalePrice', kind='line')
+    plt.show()
 
 #checkeing rmse on different values of alpha
 alphas = []
 for i in range(0,200):
-    alphas.append(i*.01+9)
+    alphas.append(i*.0000001+10.10076)
     
 def rmse_cv(model):
     rmse= np.sqrt(-cross_val_score(model, X_train, y, scoring="neg_mean_squared_error", cv = 5))
@@ -81,28 +92,63 @@ cv_ridge = [rmse_cv(Ridge(alpha = alpha)).mean()
             for alpha in alphas]
 
 cv_ridge = pd.Series(cv_ridge, index = alphas)
-#cv_ridge.plot(title = "Validation - Just Do It")
-#plt.xlabel("alpha")
-#plt.ylabel("rmse")
-#plt.show()
+cv_ridge.plot(title = "Validation - Just Do It")
+plt.xlabel("alpha")
+plt.ylabel("rmse")
+plt.show()
 
 print cv_ridge.min()
 
 alphas2 = []
-for i in range(0,10000):
-    alphas2.append(i*.0001)
+zeronorm = []
 
-#model_lasso = LassoCV(alphas = alphas).fit(X_train, y)
-model_lasso = LassoCV()
+def lnorm(mini, maxi, increment):
+    for i in range(mini,maxi):
+        alphas2.append(i*increment)
+        alphas3 = i*increment
+        #print type(alphas3)
+        model_lasso = Lasso(alpha = alphas3).fit(X_train, y)
+        coefficients = model_lasso.coef_
+        nonzeros = 0;
+        for hey in coefficients:
+            if (coefficients[hey] != 0.0):
+                nonzeros+=1
+        zeronorm.append(nonzeros)
+    #print zeronorm
+    plt.plot(alphas2, zeronorm)
+    plt.show()
+                
+lnorm(1,100,.0001)        
+#model_lasso = LassoCV()
 
 #cv_lasso = [rmse_cv(LassoCV(alphas = alpha)).mean()
 #            for alpha in alphas2]
-
+#
 #cv_lasso = pd.Series(cv_lasso, index = alphas2)
 #cv_lasso.plot(title = "Validation - Just Do It2")
 #plt.xlabel("alpha")
 #plt.ylabel("rmse")
 #plt.show()
+def findBestLasso(mini,maxi,increment):
+    bestAlpha = 0
+    currentMin = 1
+    for i in range(mini,maxi):
+        alphaTest = i*increment
+        model_lasso_test = Lasso(alpha = alphaTest).fit(X_train, y)
+        score=rmse_cv(model_lasso_test).mean()
+        #print score
+        #print currentMin
+        if(score < currentMin):
+            bestAlpha=alphaTest
+            currentMin=score
+    return bestAlpha
+bestone = findBestLasso(52550,52650,.00000001)
+print 'best lasso alpha is:'
+print bestone
+
+lassoTest(bestone)
+
+
 #print alphas2
 print 'lasso mean:'
 print rmse_cv(model_lasso).mean()
