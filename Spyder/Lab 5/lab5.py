d0c2e1996dcf0f5a9ab3817b5d08c2ac03c8499f
Atreya Misra <atreyamisra@Atreyas-MacBook-Pro.local>
2017-02-20 23:39:57 -0600

    5 finished

--- Spyder/Lab 5/lab5.py
+++ Spyder/Lab 5/lab5.py
@@ -13,6 +13,8 @@ from sklearn.metrics import confusion_matrix
 from sklearn.lda import LDA
 from sklearn.qda import QDA
 from sklearn.cluster import KMeans
+from sklearn import cross_validation
+from sklearn.metrics import mean_squared_error
 #from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
 #from sklearn.discriminant_analysis import QuadraticDiscriminantAnalysis
 
@@ -78,7 +80,7 @@ conmatrixQDA = confusion_matrix(y, qda.predict(X))
 print "QDA CONFUSION MATRIX:"
 print conmatrixQDA
 
-knn=KMeans(n_clusters=2)
+knn=KMeans(n_clusters=1)
 knn.fit(X,y)
 print "KNN ACCURACY:"
 print knn.score(X,y)
@@ -89,8 +91,72 @@ print knn.score(X,y)
 #print "QDA CONFUSION MATRIX:"
 #print conmatrixKNN
 
+#KNN is definitely the most error prone especially with just one cluster
 
+# -------------NUMBER 5 STARTING HERE ------------------------
 
+default = pd.read_excel("Data/Default.xlsx")
+y1 = default.default
+X1 = default.drop(['default', 'student'], 1)
+log1 = LogisticRegression()
+log1 = log1.fit(X1,y1)
+print "LOGISTIC ACCURACY FOR 5:"
+print log1.score(X1,y1)
+
+def CrossValidate(train, validate):
+    yt = train.default
+    Xt = train.drop(['default', 'student'], 1)
+    yv = validate.default
+    logt = LogisticRegression()
+    logt = logt.fit(Xt,yt)
+    print "LOGISTIC ACCURACY ON OWN TRAINING SET:"
+    print logt.score(Xt,yt)
+    print "LOGISTIC ACCURACY ON VALIDATE SET:"
+    print logt.score(Xt,yv)
+    pred = logt.predict(Xt)
+    #print mean_squared_error(yv, pred)
+    
+
+train1 = default[0:4999]
+validate1 = default[5000:9999]
+CrossValidate(train1, validate1)
+
+train2 = default[0:2500]
+validate2 = default[2501:5001]
+CrossValidate(train2, validate2)
+
+train3 = default[0:4000]
+validate3 = default[4001:8001]
+CrossValidate(train3, validate3)
+
+train4 = default[0:1000]
+validate4 = default[1001:2001]
+CrossValidate(train4, validate4)
+
+default['StudentBool'] = (default.student == 'Yes').astype(int)
+
+y2 = default.default
+X2 = default.drop(['default', 'student'], 1)
+log2 = LogisticRegression()
+log2 = log1.fit(X2,y2)
+print "LOGISTIC ACCURACY FOR 5 WHEN STUDENT DUMMY ADDED:"
+print log1.score(X2,y2)
+
+traind1 = default[0:4999]
+validated1 = default[5000:9999]
+CrossValidate(traind1, validated1)
+
+traind2 = default[0:2500]
+validated2 = default[2501:5001]
+CrossValidate(traind2, validated2)
+
+traind3 = default[0:4000]
+validated3 = default[4001:8001]
+CrossValidate(traind3, validated3)
+
+traind4 = default[0:1000]
+validated4 = default[1001:2001]
+CrossValidate(traind4, validated4)
 
 
 
